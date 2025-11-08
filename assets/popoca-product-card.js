51387cac703a0e467db2348beede1c7fdf865c0f
bradmallett <bmallett2@gmail.com>
2025-11-08 12:30:29 -0600

    working through product card

--- assets/popoca-product-card.js
+++ assets/popoca-product-card.js
@@ -1,6 +1,28 @@
+// @ts-nocheck 
+
     document.addEventListener('click', (e) => {
-        const btn = e.target.closest('[data-variant-id]');
+        const selectedVariantBtn = e.target.closest('[data-id]');
+        const selectedProductCard = e.target.closest('[data-popular-product-card]');
+
+        if (!selectedVariantBtn || !selectedProductCard) return;
         
-        console.log(btn)
+        const featuredImage = selectedProductCard.querySelector('[data-featured-image]');
+        const priceEl = selectedProductCard.querySelector('[data-price-el]');
+        const onSaleEl = selectedProductCard.querySelector('[data-on-sale]');
+
+        // update image to selected variant img
+        featuredImage.src = selectedVariantBtn.dataset.image;
+        featuredImage.srcset = ''; 
+
+        // update price to selected variant price
+        priceEl.textContent = selectedVariantBtn.dataset.priceMoney;
+
+        // toggle sale badge
+        const variantPrice = parseInt(selectedVariantBtn.dataset.priceCents || '0', 10);
+        const variantComparePrice = parseInt(selectedVariantBtn.dataset.comparePriceCents || '0', 10);
+        const variantIsOnSale = variantComparePrice && variantComparePrice > variantPrice;
+
+        if (onSaleEl) onSaleEl.classList.toggle('hidden', !variantIsOnSale);
+
 
     })
