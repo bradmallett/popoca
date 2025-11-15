f0f885c93b8460896cecbad9d3075e515549af7b
bradmallett <bmallett2@gmail.com>
2025-11-15 14:25:57 -0600

    adding low stock badges

--- assets/popoca-product-card.js
+++ assets/popoca-product-card.js
@@ -12,6 +12,8 @@
         const onSaleEl = selectedProductCard.querySelector('[data-on-sale]');
         const productLinkEl = selectedProductCard.querySelectorAll('[data-product-link]');
         const addToCartEl = selectedProductCard.querySelector('[data-add-variant-to-cart]');
+        const percentOffEl = selectedProductCard.querySelector('[data-percent-off]');
+        const lowStockEl = selectedProductCard.querySelector('[data-low-stock]');
 
         // update image to selected variant img
         featuredImage.src = selectedVariantBtn.dataset.image;
@@ -35,8 +37,16 @@
         const variantComparePrice = parseInt(selectedVariantBtn.dataset.compareCents || '0', 10);
         const variantIsOnSale = variantComparePrice && variantComparePrice > variantPrice;
 
-        if (onSaleEl) onSaleEl.classList.toggle('hidden', !variantIsOnSale);
+        if (onSaleEl) {
+            const percentageOff = 100 - Math.round((variantPrice / variantComparePrice) * 100);
+            percentOffEl.textContent = percentageOff + '% OFF';
+            onSaleEl.classList.toggle('hidden', !variantIsOnSale);
+        }
 
+        // toggle low stock badge
+        const inventory = parseInt(selectedVariantBtn.dataset.variantInventory || '0', 10);
+        const inventoryIsLow = inventory <= 5;
+        lowStockEl.classList.toggle('hidden', !inventoryIsLow)
 
 
         addToCartEl.value = selectedVariantBtn.dataset.variantId;
